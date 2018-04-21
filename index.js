b0f7e6881209cbfc8a351c0f17bc31434e6a4de2
Johnatan <johnatan@johnatans-MacBook-Air-2.local>
2018-04-21 14:32:41 -0400

    exercise

---
+++ index.js
@@ -0,0 +1,17 @@
+// $("p").click(function(){
+
+}
+
+$(".red").on("click", function(){
+  $(this).fadeOut(3000);
+
+});
+
+$("li").on("click, function"(){
+    console.log("this is li: ", $(this).html())
+})
+
+//  $("button").click(Function (){
+//      $("input").toggle();
+//  })
+
